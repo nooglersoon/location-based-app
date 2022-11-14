2fd71c58b3e32efc52e8318b46b5c38164156855
nooglersoon <nooglersoon@outlook.co.id>
2022-11-14 22:39:02 +0700

    Initial Commit

---
+++ LocationData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LocationData
+//
+//  Created by Fauzi Achmad B D on 14/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
